102362cfa5da4382a847a9eb2b03d0aa7fe486d1
fallen8angel <75917889+fallen8angel@users.noreply.github.com>
2026-06-23 17:38:52 +0900

    Restore Hyundai button helpers after starting state change

--- opendbc_repo/opendbc/car/hyundai/interface.py
+++ opendbc_repo/opendbc/car/hyundai/interface.py
@@ -259,3 +259,36 @@ class CarInterface(CarInterfaceBase):
     if CP.flags & HyundaiFlags.CANFD:
       return [16], [20]
     return [16, 20], [12, 14, 16, 18]
+
+  def create_buttons(self, button):
+    if self.CP.flags & HyundaiFlags.CANFD:
+      if self.CP.flags & HyundaiFlags.CANFD_ALT_BUTTONS:
+        return self.create_buttons_can_fd_alt(button)
+      return self.create_buttons_can_fd(button)
+    else:
+      return self.create_buttons_can(button)
+
+  def get_buttons_dict(self):
+    return BUTTONS_DICT
+
+  def create_buttons_can(self, button):
+    values = copy.copy(self.CS.clu11)
+    values["CF_Clu_CruiseSwState"] = button
+    values["CF_Clu_AliveCnt1"] = (values["CF_Clu_AliveCnt1"] + 1) % 0x10
+    return self.CC.packer.make_can_msg("CLU11", self.CP.sccBus, values)
+
+  def create_buttons_can_fd(self, button):
+    values = {
+      "COUNTER": self.CS.buttons_counter + 1,
+      "SET_ME_1": 1,
+      "CRUISE_BUTTONS": button,
+    }
+    bus = self.CC.CAN.ECAN if self.CP.flags & HyundaiFlags.CANFD_LKA_STEERING else self.CC.CAN.CAM
+    return self.CC.packer.make_can_msg("CRUISE_BUTTONS", bus, values)
+
+  def create_buttons_can_fd_alt(self, button):
+    values = copy.copy(self.CS.canfd_buttons)
+    values["CRUISE_BUTTONS"] = button
+    values["COUNTER"] = (values["COUNTER"] + 1) % 256
+    bus = self.CC.CAN.ECAN if self.CP.flags & HyundaiFlags.CANFD_LKA_STEERING else self.CC.CAN.CAM
+    return self.CC.packer.make_can_msg("CRUISE_BUTTONS_ALT", bus, values)
